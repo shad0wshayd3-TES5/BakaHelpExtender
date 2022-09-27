35e64b2084440135e8adefa9128a681661f7082b
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2022-09-27 04:14:30 -0600

    skip trying po3_tweaks cache for uncached formtypes

--- src/c++/ObScript/Help.h
+++ src/c++/ObScript/Help.h
@@ -44,15 +44,46 @@ namespace ObScript
 		public:
 			static const char* GetFormEditorID(const RE::TESForm* a_form)
 			{
-				auto hndl = GetModuleHandleA("po3_Tweaks");
-				auto func = reinterpret_cast<const char* (*)(std::uint32_t)>(GetProcAddress(hndl, "GetFormEditorID"));
-				if (func)
+				switch (a_form->GetFormType())
 				{
-					std::string result{ func(a_form->formID) };
-					if (!result.empty())
-					{
-						return result.c_str();
-					}
+					case RE::FormType::Keyword:
+					case RE::FormType::LocationRefType:
+					case RE::FormType::Action:
+					case RE::FormType::MenuIcon:
+					case RE::FormType::Global:
+					case RE::FormType::HeadPart:
+					case RE::FormType::Race:
+					case RE::FormType::Sound:
+					case RE::FormType::Script:
+					case RE::FormType::Navigation:
+					case RE::FormType::Cell:
+					case RE::FormType::WorldSpace:
+					case RE::FormType::Land:
+					case RE::FormType::NavMesh:
+					case RE::FormType::Dialogue:
+					case RE::FormType::Quest:
+					case RE::FormType::Idle:
+					case RE::FormType::AnimatedObject:
+					case RE::FormType::ImageAdapter:
+					case RE::FormType::VoiceType:
+					case RE::FormType::Ragdoll:
+					case RE::FormType::DefaultObject:
+					case RE::FormType::MusicType:
+					case RE::FormType::StoryManagerBranchNode:
+					case RE::FormType::StoryManagerQuestNode:
+					case RE::FormType::StoryManagerEventNode:
+					case RE::FormType::SoundRecord:
+						break;
+
+					default:
+						{
+							auto hndl = GetModuleHandleA("po3_Tweaks");
+							auto func = reinterpret_cast<const char* (*)(std::uint32_t)>(GetProcAddress(hndl, "GetFormEditorID"));
+							if (func)
+							{
+								return func(a_form->formID);
+							}
+						}
 				}
 
 				return a_form->GetFormEditorID();
