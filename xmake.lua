a8ba41874e442f28460216a2bbc30f791812bf63
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2026-08-20 20:53:08 -0600

    refactor: project update

--- xmake.lua
+++ xmake.lua
@@ -1,35 +1,19 @@
--- set minimum xmake version
-set_xmakever("2.8.2")
-
--- includes
+-- include subprojects
 includes("lib/commonlibsse")
 
--- set project
+-- set project constants
 set_project("HelpExtender")
-set_version("2.0.0")
+set_version("3.0.0")
 set_license("GPL-3.0")
-
--- set defaults
 set_languages("c++23")
 set_warnings("allextra")
 
--- add rules
+-- add common rules
 add_rules("mode.debug", "mode.releasedbg")
 add_rules("plugin.vsxmake.autoupdate")
 
--- set policies
-set_policy("build.optimization.lto", true)
-set_policy("package.requires_lock", true)
-
--- set configs
-set_config("skyrim_ae", true)
-
--- targets
+-- define targets
 target("HelpExtender")
-    -- add dependencies to target
-    add_deps("commonlibsse")
-
-    -- add commonlibsse plugin
     add_rules("commonlibsse.plugin", {
         name = "HelpExtender",
         author = "shad0wshayd3"
